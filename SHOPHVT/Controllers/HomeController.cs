dd1dfd7c9c727b7d0f72fbe92d5d1b8d7b047703
HoangLongUTE <21115053120123ute@gmail.com>
2023-12-26 08:47:28 +0700

    update home

--- SHOPHVT/Controllers/HomeController.cs
+++ SHOPHVT/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using SHOPHVT.Models;
 
 namespace SHOPHVT.Controllers
 {
@@ -14,9 +16,39 @@ namespace SHOPHVT.Controllers
        
         private readonly UserManager<IdentityUser> _userManager;
 
-        public ActionResult Index()
+        public ActionResult Index(int? categoryId)
         {
-            return View();
+            // Lấy danh sách danh mục
+            var categories = _db.DanhMuc.ToList();
+
+            if (HttpContext.Request.HttpMethod == "POST")
+            {
+                return RedirectToAction("Index", new { categoryId });
+            }
+
+            // Lấy danh sách sản phẩm theo danh mục được chọn
+            List<Product> products;
+            if (categoryId == null || categoryId == 0)
+            {
+                // Gọi stored procedure ProcGetSanPham
+                products = _db.Database.SqlQuery<Product>("EXEC ProcGetSanPham @sanPhamID=NULL").ToList();
+
+                // Mặc định chọn danh mục "All" khi trang được load
+                ViewBag.SelectedCategory = null;
+            }
+            else
+            {
+                // Gọi stored procedure ProcGetSanPhamDanhMuc
+                var parameter = new SqlParameter("@danhMucID", categoryId);
+                products = _db.Database.SqlQuery<Product>("EXEC ProcGetSanPhamDanhMuc @danhMucID", parameter).ToList();
+
+                // Thiết lập giá trị của danh mục được chọn
+                ViewBag.SelectedCategory = categoryId;
+            }
+
+            // Truyền danh sách danh mục và sản phẩm đến view
+            ViewBag.Categories = categories;
+            return View(products);
         }
 
         public ActionResult About()
